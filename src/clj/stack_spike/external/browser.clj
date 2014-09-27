569cc923f90ed701d938ac632710eb7e06dc4602
Brian Jenkins <bonkydog@bonkydog.com>
2014-09-27 16:23:13 -0700

    Rename browser to driver.

--- src/clj/stack_spike/external/browser.clj
+++ src/clj/stack_spike/external/browser.clj
@@ -9,24 +9,24 @@
 (defrecord Browser [browser web]
   component/Lifecycle
   (start [component]
-    (let [browser (new-driver {:browser :firefox})]
-      (implicit-wait browser 3000)
-      ;;(.addShutdownHook (Runtime/getRuntime) (Thread. #(quit browser)))
-      (assoc component :web web, :browser browser)))
+    (let [driver (new-driver {:browser :firefox})]
+      (implicit-wait driver 3000)
+      ;;(.addShutdownHook (Runtime/getRuntime) (Thread. #(quit driver)))
+      (assoc component :web web, :driver driver)))
   (stop [component]
-    (quit (:browser component))
-    (dissoc component :browser :web)))
+    (quit (:driver component))
+    (dissoc component :driver :web)))
 
 (defn new-browser []
   (map->Browser {}))
 
 (defn visit [browser path]
-  (to (:browser browser) (str (web/root-url (:web browser)) (s/replace-first path #"^/" ""))))
+  (to (:driver browser) (str (web/root-url (:web browser)) (s/replace-first path #"^/" ""))))
 
 (defn assert-text-present [browser expected-content]
-  (let [actual-content (text (find-element (:browser browser) {:css "body"}))]
+  (let [actual-content (text (find-element (:driver browser) {:css "body"}))]
     (is (<= 0
-           (.indexOf
-            actual-content
-            expected-content))
-	    (str "expected to see \"" expected-content "\" in \"" actual-content "\""))))
+            (.indexOf
+             actual-content
+             expected-content))
+        (str "expected to see \"" expected-content "\" in \"" actual-content "\""))))
