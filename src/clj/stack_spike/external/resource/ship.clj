150a851d6df29acfa95c618280c8094b6279754a
Brian Jenkins <bonkydog@bonkydog.com>
2014-10-09 15:05:24 -0700

    Extract markup generation to its own namespace.

--- src/clj/stack_spike/external/resource/ship.clj
+++ src/clj/stack_spike/external/resource/ship.clj
@@ -1,22 +1,11 @@
 (ns stack-spike.external.resource.ship
   (:require [liberator.core :refer [defresource]]
-            [hiccup.core :refer [html]]))
+            [stack-spike.external.html.ship :as html]))
 
 (defresource ship
   :available-media-types ["text/html"]
-  :handle-ok (fn [req] (html [:html
-                           [:body
-                            [:p (str "Hello, this is the ship page for "
-                                     (get-in req [:request :params :id]))]]])))
+  :handle-ok (fn [req] (html/show (get-in req [:request :params :id]))))
 
 (defresource ship-list
   :available-media-types ["text/html"]
-  :handle-ok (fn [_] (html [:html
-                           [:body
-                            [:table {:class "ships"}
-                             [:thead
-                              [:tr
-                               [:th "name"]]
-                              [:tbody
-                               [:tr
-                                [:td "foo"]]]]]]])))
+  :handle-ok (fn [req] (html/index)))
