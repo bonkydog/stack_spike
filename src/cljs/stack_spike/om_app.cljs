bc1bdb278ffbe4680b34361ae0bb4d991066ebf5
Brian Jenkins <bonkydog@bonkydog.com>
2015-01-19 16:37:14 -0800

    Fetch ships from server for Om index page.

--- src/cljs/stack_spike/om_app.cljs
+++ src/cljs/stack_spike/om_app.cljs
@@ -1,24 +1,26 @@
 (ns stack-spike.om-app
+  (:require-macros [cljs.core.async.macros :refer [go alt!]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! <! >! chan timeout]]
+            [cljs-http.client :as http]
             [clojure.browser.repl]
-            [stack-spike.tools :refer [log]])))
+            [stack-spike.tools :refer [log]]))
 
-(ws-repl/connect "ws://localhost:9001")
+(enable-console-print!)
 
 (def app-state
   (atom {:page "index"
-         :ships [{:id 23 :name "foo"}
-                 {:id 17 :name "bar"}]}))
+         :ships []}))
 
 (defn ship-row [ship]
   (om/component
    (dom/tr {:id (str "ship-" (:id ship)) :className "ship"}
            (dom/td {:id "id"}
-                   (dom/a {:href (str "/ship/" (:id ship)) :className "edit"}
-                          (:id ship)))
+                   (dom/a {:href (str "/ship/" (:db/id ship)) :className "edit"}
+                          (:db/id ship)))
            (dom/td {:className "id"}
-                   (:name ship)))))
+                   (:ship/name ship)))))
 
 (defn ships-index [app owner]
   (reify
@@ -26,7 +28,19 @@
     (render [this]
       (dom/table {:className "ships"}
                  (apply dom/tbody nil
-                        (om/build-all ship-row (:ships app)))))))
+                        (om/build-all ship-row (:ships app)))))
+    om/IWillMount
+    (will-mount [this]
+      (fetch-ships app))))
+
+(defn fetch-ships [app]
+  (go (let [response (<! (http/get "/ships" {:headers {"Accept" "application/transit+json;verbose"}}))]
+        (prn (:status response))
+        (prn (:body response))
+        (prn app)
+        (om/update! app :ships (vec (:body response)))
+        (prn @app)
+        )))
 
 (om/root ships-index
          app-state
