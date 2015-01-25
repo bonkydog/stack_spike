6c5710c854ef0b1fcfe72ee608af8674068dddb5
Brian Jenkins <bonkydog@bonkydog.com>
2015-01-24 22:37:10 -0800

    Fix logging.

--- project.clj
+++ project.clj
@@ -33,7 +33,9 @@
                  [bidi "1.15.0" :exclusions [org.clojure/clojure]]
                  [liberator "0.12.3-SNAPSHOT"]
                  [hiccup "1.0.5"]
-                 [com.datomic/datomic-pro "0.9.5078"]
+                 [com.datomic/datomic-pro "0.9.5078"
+                  :exclusions [org.slf4j/slf4j-nop org.slf4j/log4j-over-slf4j]]
+                 [org.slf4j/slf4j-log4j12 "1.7.10"]
                  [com.stuartsierra/component "0.2.2"]
                  [clj-webdriver "0.6.1"]
                  [cljs-http "0.1.24"]
