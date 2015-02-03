3f7339a4844209af9b10a9b8a649c1f4f5478525
Brian Jenkins <bonkydog@bonkydog.com>
2015-02-02 16:30:13 -0800

    Rip out lame state code.

--- src/cljs/stack_spike/om_app.cljs
+++ src/cljs/stack_spike/om_app.cljs
@@ -1,6 +1,7 @@
 (ns stack-spike.om-app
   (:require-macros [cljs.core.async.macros :refer [go go-loop alt!]])
   (:require [om.core :as om :include-macros true]
+            [om-sync.core :as sync]
             [om-tools.dom :as dom :include-macros true]
             [om.dom]
             [cljs.core.async :refer [put! <! >! chan timeout]]
@@ -27,76 +28,63 @@
          :ships nil}))
 
 
-(defn navigate [owner event]
+(defn set-page [path]
+  (om/update! (om/root-cursor app-state) :page (resolve path)))
+
+(defn goto [url]
+  (.pushState js/history {} nil url )
+  (set-page url))
+
+(defn navigate [event]
   (.preventDefault event)
-  (put! (om/get-shared owner :nav-chan) (.-href (.-target event)))
+  (goto (-> event .-target .-href))
   nil)
 
 
 (defn ship-row [id-ship-pair owner]
   (reify
-    om/IInitState
-    (init-state [_] (last id-ship-pair))
-    om/IRenderState
-    (render-state [this state] 
+    om/IRender
+    (render [this]
       (let [ship (last id-ship-pair)]
         (dom/tr {:id (str "ship-" (:db/id ship)) :class "ship"}
                 (dom/td {:class "id"}
                         (dom/a {:href (str "/ships/" (:db/id ship))
                                 :class "edit"
-                                :on-click (partial navigate owner)}
+                                :on-click navigate}
                                (:db/id ship)))
                 (dom/td {:class "name"}
                         (:ship/name ship))
                 (dom/td {:class "controls"}
-                        (dom/a {:class "delete"
-                                :href "#"
-                                :on-click (fn [e]
-                                            (.preventDefault e)
-                                            (put! (om/get-shared owner :delete-chan) state)
-                                            nil)}
+                        (dom/a {:class "delete" :href "#"}
                                "[delete]")))))))
-
-(defn handle-change [e owner {:keys [name]}]
-  (.preventDefault e)
-  (println "handling change!")
-  (om/set-state! owner :ship/name (.. e -target -value)))
-
-
-
 
 (defn ship [ship owner]
   (reify
     om/IInitState
-    (init-state [_]
-      {:db/id (:db/id ship) :ship/name (:ship/name ship)})
+    (init-state [this]
+      ship)
+
     om/IRenderState
     (render-state [this state]
-      (dom/form {:class "ship" :method "POST" :on-submit (fn [e]
-                                                           (.preventDefault e)
-                                                           (put! (om/get-shared owner :update-chan) state )
-                                                           nil
-                                                           )}
+      (dom/form {:class "ship" :method "POST"}
                 (dom/label {:for "name"} "Name")
-                (dom/input {:id "name" :type "text" :name "name" :value (:ship/name state) :autofocus "autofocus"
-                            :on-change #(handle-change % owner state)})
+                (dom/input {:id "name" :type "text" :name "name" :value (:ship/name state) :auto-focus true
+                            :on-change #(om/set-state! owner :ship/name (-> % .-target .-value))})
                 (dom/input {:type "submit" :value "Update Ship"})))))
 
 
 (defn new-ship [ship owner]
   (reify
     om/IInitState
-    (init-state [_]
-      {:db/id (:db/id ship) :ship/name (:ship/name ship)})
+    (init-state [this]
+      ship)
+
     om/IRenderState
     (render-state [this state]
-      (dom/form {:class "ship" :method "POST" :on-submit (fn [e]
-                                                           (.preventDefault e)
-                                                           (put! (om/get-shared owner :create-chan) state )
-                                                           nil)}
+      (dom/form {:class "ship" :method "POST"}
                 (dom/label {:for "name"} "Name")
-                (dom/input {:id "name" :type "text" :name "name" :value (:ship/name state) :autofocus "autofocus"
-                            :on-change #(handle-change % owner state)})
+                (dom/input {:id "name" :type "text" :name "name" :value (:ship/name state) :auto-focus true
+                            :on-change #(om/set-state! owner :ship/name (-> % .-target .-value))})
                 (dom/input {:type "submit" :value "Create Ship"})))))
 
 (defn ships [ships owner]
@@ -110,7 +98,7 @@
                      (dom/th "name"))
                     (dom/tbody
                      (om/build-all ship-row ships)))
-               (dom/a {:class "new-ship" :href "/ships/new" :on-click (partial navigate owner) } "New Ship")))
+               (dom/a {:class "new-ship" :href "/om/ships/new" :on-click navigate } "New Ship")))
     ))
 
 (defn loading []
@@ -154,13 +142,6 @@
     (will-mount [this]
       #_(fetch-ships app))))
 
-(defn set-page [path]
-  (om/update! (om/root-cursor app-state) :page (resolve path)))
-
-(defn goto [url]
-  (.pushState js/history {} nil url )
-  (set-page url))
-
 (def csrf-token
   (if (exists? js/document)
     (.-content (.getElementById js/document "csrf-token"))
@@ -174,49 +155,11 @@
          app-state)
 
 (defn main []
-  (let [nav-chan (chan)
-        update-chan (chan)
-        create-chan (chan)
-        delete-chan (chan)]
-    (go-loop []
-      (alt! nav-chan ([url]
-                      (log "nav-chan: " url)
-                      (goto url)
-                      (log "nav-chan complete!"))
-            update-chan ([ship-update]
-                         (http/post "/api/action"
-                                    {:transit-params [:update-ship (de-namespace-keys ship-update)]
-                                     :headers {"Accept" "application/transit+json;verbose"
-                                               "X-CSRF-Token" csrf-token}})
-                         (om/update! (om/root-cursor app-state) [:ships (:db/id ship-update)] ship-update)
-                         (goto "/ships"))
-            create-chan ([ship-creation]
-                         (let [response (<! (http/post "/api/action"
-                                                       {:transit-params [:create-ship (de-namespace-keys  ship-creation)]
-                                                        :headers {"Accept" "application/transit+json;verbose"
-                                                                  "X-CSRF-Token" csrf-token}}))]
-                           (om/update! (om/root-cursor app-state) [:ships (:db/id (get-in response [:body :result]))] (get-in response [:body :result]))
-                           (goto "/ships")))
-            delete-chan ([ship-deletion]
-                         (http/post "/api/action"
-                                    {:transit-params [:delete-ship (de-namespace-keys ship-deletion)]
-                                     :headers {"Accept" "application/transit+json;verbose"
-                                               "X-CSRF-Token" csrf-token}})
-                         (om/transact! (om/root-cursor app-state) :ships #(dissoc % (:db/id ship-deletion)))))
-
-      (recur))
-
-    (set! (.-onpopstate js/window) (fn [e]
-                                     (log e)
-                                     (log "pop!")
-                                     (set-page (current-url))))
-    (om/root page
-             app-state
-             {:target app-container
-              :shared {:nav-chan nav-chan
-                       :update-chan update-chan
-                       :create-chan create-chan
-                       :delete-chan delete-chan}})))
+  (set! (.-onpopstate js/window) (fn [e]
+                                   (set-page (current-url))))
+  (om/root page
+           app-state
+           {:target (. js/document (getElementById "root"))}))
 
 
 (defn ^:export render-to-string
