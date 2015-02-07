3307823ef0055557d3ed2ad4507df0c850389482
Brian Jenkins <bonkydog@bonkydog.com>
2015-02-07 10:17:32 -0800

    Pool server-side React runtimes.

--- src/clj/stack_spike/interface/html/isomorphic.clj
+++ src/clj/stack_spike/interface/html/isomorphic.clj
@@ -12,20 +12,33 @@
       io/resource
       io/reader))
 
-(defn render [edn]
+(defn- render-fn* []
   (try (.eval js "var global = this")
        (.eval js (resource "main-iso.js"))
        (let [view (.eval js "stack_spike.om_app")]
-         (.invokeMethod
-          ^Invocable js
-          view
-          "render_to_string"
-          (-> edn
-              list
-              object-array)))
+         (fn [edn]
+           (.invokeMethod
+            ^Invocable js
+            view
+            "render_to_string"
+            (-> edn
+                list
+                object-array))))
        (catch Exception e
          (prn e)
          (println (.getFileName e))
          (println (.getLineNumber e)))))
 
 
+(defn render-fn
+  []
+  (let [pool (ref (repeatedly 3 render-fn*))]
+    (fn render [state-edn]
+      (let [rendr (dosync
+                   (let [f (first @pool)]
+                     (alter pool rest)
+                     f))
+            rendr (or rendr (render-fn*))
+            html (rendr state-edn)]
+        (dosync (alter pool conj rendr))
+        html))))
